227629f30b173edf5bc198097c85d5c7a0ee186c
hwb2017 <1003673802@qq.com>
2021-07-25 23:54:31 +0800

    增加Vue数据驱动过程中initState代码的解析

---
+++ vue2.x/demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
